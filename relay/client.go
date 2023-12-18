456b71b1512f2779c6dabe6a00c7d7f842a57076
Jun Kimura <jun.kimura@datachain.jp>
2023-12-18 12:28:26 +0900

    fix to initialize grpc connection in prover constructor
    Signed-off-by: Jun Kimura <jun.kimura@datachain.jp>

--- relay/client.go
+++ relay/client.go
@@ -1,7 +1,6 @@
 package relay
 
 import (
-	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/datachainlab/lcp-go/relay/elc"
 	"github.com/datachainlab/lcp-go/relay/enclave"
 	"google.golang.org/grpc"
@@ -14,16 +13,13 @@ type (
 )
 
 type LCPServiceClient struct {
-	codec codec.ProtoCodecMarshaler
-
 	ELCMsgClient
 	ELCQueryClient
 	EnclaveQueryClient
 }
 
-func NewLCPServiceClient(conn *grpc.ClientConn, codec codec.ProtoCodecMarshaler) LCPServiceClient {
+func NewLCPServiceClient(conn *grpc.ClientConn) LCPServiceClient {
 	return LCPServiceClient{
-		codec:              codec,
 		ELCMsgClient:       elc.NewMsgClient(conn),
 		ELCQueryClient:     elc.NewQueryClient(conn),
 		EnclaveQueryClient: enclave.NewQueryClient(conn),
